9954b3a10fb1e6a0eb1eeedf4a7af4512abcaea9
Annie  Lin <https://github.com/linannie/linannie.github.io/commit/7c494e04c2cf7492290aecd4a064ccb7e3416ee6.patch>
2018-01-10 11:13:49 -0800

    songs

---
+++ script.js
@@ -0,0 +1,115 @@
+function happy(){
+	var songs = ["https://open.spotify.com/track/3FnkZFv7kbxwlgaLgbqeDc",
+				 "https://open.spotify.com/track/5VnDkUNyX6u5Sk0yZiP8XB",
+				 "https://open.spotify.com/track/2PpruBYCo4H7WOBJ7Q2EwM",
+				 "https://open.spotify.com/track/1eyzqe2QqGZUmfcPZtrIyt"
+
+				 ];
+	var randomNumber = Math.floor(Math.random() * (songs.length));
+	var song = songs[randomNumber];
+	window.open(song);
+
+
+
+}
+function sad(){
+	var songs = ["https://open.spotify.com/track/1yZFWn5IOy4spqLgJmMLmU",
+				 "https://open.spotify.com/track/4WM22pqXRZkcdrp7alhjsv",
+				 "https://open.spotify.com/track/7LVHVU3tWfcxj5aiPFEW4Q",
+				 "https://open.spotify.com/track/7ve7yWufzZb92j6XlzQtw7",
+				 "https://open.spotify.com/track/6mFkJmJqdDVQ1REhVfGgd1",
+				 "https://open.spotify.com/track/41pFyxOpS119Pb8JEzhx0w",
+				 "https://open.spotify.com/track/4yugZvBYaoREkJKtbG08Qr",
+				 "https://open.spotify.com/track/476JcJ3TpAuCOlGdCmomGm"
+
+
+
+	];
+	var randomNumber = Math.floor(Math.random() * (songs.length));
+	var song = songs[randomNumber];
+	window.open(song);
+}
+
+function angry(){
+	var songs = ["https://open.spotify.com/track/10zycm3kRmxg5etQ8Qhpq4",
+			     "https://open.spotify.com/track/7KcGEssn7BnJdTgildK5y0",
+			     "https://open.spotify.com/track/7Lf7oSEVdzZqTA0kEDSlS5",
+			     "https://open.spotify.com/track/11dxtPJKR4E0wlSr0A0t47",
+			     "https://open.spotify.com/track/3rJkayQHY0CCYMb8cCuvpk",
+			     "https://open.spotify.com/track/2p4p9YGwmJIdf5IA9sSWhm",
+			     "https://open.spotify.com/track/1R08nQA87DklgMg9h0nVK5",
+			     "https://open.spotify.com/track/3Q3myFA7q4Op95DOpHplaY",
+			     "https://open.spotify.com/track/0qgZvYWLIqDn0tXimF8gPa"
+
+
+				];
+	var randomNumber = Math.floor(Math.random() * (songs.length));
+	var song = songs[randomNumber];
+	window.open(song);
+
+}
+
+function hyped(){
+	var songs = ["https://open.spotify.com/track/1OxcIUqVmVYxT6427tbhDW",
+				"https://open.spotify.com/track/2AT8iROs4FQueDv2c8q2KE",
+				"https://open.spotify.com/track/2tbvAGoLMQgjvfgW7naBiE",
+				"https://open.spotify.com/track/0gpFiquPvir7e3RrM4kii9",
+				"https://open.spotify.com/track/30d50iqspscgtULK3QcEpf",
+				"https://open.spotify.com/track/1fnlUPzKQ0DnChntJy08VN",
+				"https://open.spotify.com/track/6GGWa2HbqBo8q0DdhYvb0V",
+				"https://open.spotify.com/track/1gV0hgMNdpSWeW7ZjSUKnX",
+				"https://open.spotify.com/track/0kD586ste6xyDRqUYhVlCh",
+				"https://open.spotify.com/track/2NAZcL9tLUkdMMb5v3xFVG",
+				"https://open.spotify.com/track/2KU0RHzEYq8GLwDTyimWGr",
+				"https://open.spotify.com/track/6hJSH3jS9jG6khVclppBJw",
+				"https://open.spotify.com/track/6Jao8qdCB3VYG4PhPzCEIm",
+				"https://open.spotify.com/track/2HJQcyUpmUuvzS5vBAICIc",
+				"https://open.spotify.com/track/6y3ijys1nhU1KCpy4mGED8",
+				"https://open.spotify.com/track/4lqEFIB9YZl8BwR99L5H3b",
+				"https://open.spotify.com/track/2BF1X5nkYYSb0AUTRaGkDm",
+				"https://open.spotify.com/track/4ipnJyDU3Lq15qBAYNqlqK"
+				];
+	var randomNumber = Math.floor(Math.random() * (songs.length));
+	var song = songs[randomNumber];
+	window.open(song);
+}
+
+function chill(){
+	var songs = ["https://open.spotify.com/track/6znSbsDDFdBZaiucjUGaWy",
+				 "https://open.spotify.com/track/2btKtacOXuMtC9WjcNRvAA",
+				 "https://open.spotify.com/track/30y26KfyDlsSbslOg9SC2q",
+				 "https://open.spotify.com/track/3WRQUvzRvBDr4AxMWhXc5E",
+				 "https://open.spotify.com/track/2EEZdCeGp095urJDE3wEpS",
+				 "https://open.spotify.com/track/7jZHUhAmW5oq1cq6s8IxmK",
+				 "https://open.spotify.com/track/4aJnMRNeuDZSQN7FaSNGtM",
+				 "https://open.spotify.com/track/3xhR3mClWXydDCByJxnOwY",
+				 "https://open.spotify.com/track/7D0t8BHJxakSnO6EVBglc4",
+				 "https://open.spotify.com/track/2IUHcSxFPDKLRVgjXxvKjl",
+				 "https://open.spotify.com/track/06JRkix5RRMRIWffxZqlGt",
+				 "https://open.spotify.com/track/5jnp8E61wcoF6qIEtDHQnJ",
+				 "https://open.spotify.com/track/1LeItUMezKA1HdCHxYICed",
+				 "https://open.spotify.com/track/0vtX8UMG38p7IXpP4lZJ2z"
+
+
+				];
+	var randomNumber = Math.floor(Math.random() * (songs.length));
+	var song = songs[randomNumber];
+	window.open(song);
+}
+
+function idk(){
+
+	var songs = ["https://open.spotify.com/track/5ONbCAFFnAJ0YGClEeddcU",
+				 "https://open.spotify.com/track/6ybViy2qrO9sIi41EgRJgx",
+				 "https://open.spotify.com/track/2a5Ml671R56tzRB0WMaCfC",
+				 "https://open.spotify.com/track/6b2oQwSGFkzsMtQruIWm2p",
+				 "https://open.spotify.com/track/0JXXNGljqupsJaZsgSbMZV",
+				 "https://open.spotify.com/track/16Me2jlMlTX4xsps2j71mC",
+				 "https://open.spotify.com/track/1CGMbXotw37W2TGOc16Kdj"
+
+
+				];
+	var randomNumber = Math.floor(Math.random() * (songs.length));
+	var song = songs[randomNumber];
+	window.open(song);
+}
