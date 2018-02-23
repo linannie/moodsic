629cd772a050dd917178ff7b3a720c2fcb7999bc
Annie Lin <a.popcorn2@gmail.com>
2018-02-23 13:41:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function happy(){
-	var songs = ["https://open.spotify.com/track/3FnkZFv7kbxwlgaLgbqeDc",
-				 "https://open.spotify.com/track/5VnDkUNyX6u5Sk0yZiP8XB",
+	var songs = ["https://open.spotify.com/track/3FnkZFv7kbxwlgaLgbqeDc",			// Jack Johnson sleep through the Static
+				 "https://open.spotify.com/track/5VnDkUNyX6u5Sk0yZiP8XB",	// Thunder Imagine Dragons
 				 "https://open.spotify.com/track/2PpruBYCo4H7WOBJ7Q2EwM",
-				 "https://open.spotify.com/track/1eyzqe2QqGZUmfcPZtrIyt"
+				 "https://open.spotify.com/track/4NtUY5IGzHCaqfZemmAu56?si=PV8XpvyfQhylcTAOr0yBrw",
+		     		 "https://open.spotify.com/track/3gdewACMIVMEWVbyb8O9sY?si=WlnZFzbwS2S_Lf-i2BfPnQ",
+		     		 "https://open.spotify.com/track/6FLNeNUhazLGqiScpmcmP5?si=4LVKN7PVTaOsd75TlseiSA",
+		     		 "https://open.spotify.com/track/2TVxnKdb3tqe1nhQWwwZCO?si=kWpuIQB-TPW2uvSis51F9w",
+		     		 "https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P?si=nTLIh2zkSqCtgX5cLLoYEg",
+		     		 "https://open.spotify.com/track/71T7xF5wLbDRg76jxAwDqA?si=fOYacs8iTvSuCCYqK9NQLg",
+		                 "https://open.spotify.com/track/5Q0Nhxo0l2bP3pNjpGJwV1?si=-yLsHhvHSSScINx4Q14QMA",
+		     		 "https://open.spotify.com/track/2XW4DbS6NddZxRPm5rMCeY?si=HBa30-qtTVaVAVqDYstzEQ",
+		     		 "https://open.spotify.com/track/1zi7xx7UVEFkmKfv06H8x0?si=dgWxiCgfQM-jJGC8530Vfg",
+		     		 "https://open.spotify.com/track/5mCPDVBb16L4XQwDdbRUpz?si=8CKvMURUSFyOQBMuziN0Jg",
+		     		 "https://open.spotify.com/track/2ZWlPOoWh0626oTaHrnl2a?si=zP59mlNnRm-5wmCfjdIUjw"
 
 				 ];
 	var randomNumber = Math.floor(Math.random() * (songs.length));
@@ -20,7 +30,14 @@ function sad(){
 				 "https://open.spotify.com/track/6mFkJmJqdDVQ1REhVfGgd1",
 				 "https://open.spotify.com/track/41pFyxOpS119Pb8JEzhx0w",
 				 "https://open.spotify.com/track/4yugZvBYaoREkJKtbG08Qr",
-				 "https://open.spotify.com/track/476JcJ3TpAuCOlGdCmomGm"
+				 "https://open.spotify.com/track/476JcJ3TpAuCOlGdCmomGm",
+		     		 "https://open.spotify.com/track/41pFyxOpS119Pb8JEzhx0w?si=eVc-gz75QU6InEvocLGlJA",
+		     	         "https://open.spotify.com/track/4yugZvBYaoREkJKtbG08Qr?si=HoegfBxqSGmz3HpIbt1Jww",
+		                 "https://open.spotify.com/track/2TjnCxxQRYn56Ye8gkUKiW?si=lv9Z2IS4Qfq7GX3DvEXUHw",
+		     		 "https://open.spotify.com/track/5kGBSOrKYmhRfdPBw4xD8D?si=Jxho-CrDQqqsoYE7B3075Q",
+		     		 "https://open.spotify.com/track/2xAiwGGuaqdEklu33zEXr9?si=-3PCLsLGTvGuuQv53DTPyg",
+		     	         "https://open.spotify.com/track/1BnwtbvZibyzmxlLd4ZiXB?si=kjm4MHwvTPmHqhfHrSz4Dw",
+		     		 "https://open.spotify.com/track/2YcmCjuUQbLlmhTMaQs2oE?si=j7R6uIFORPq4QpOjeE2XPQ"
 
 
 
@@ -39,7 +56,10 @@ function angry(){
 			     "https://open.spotify.com/track/2p4p9YGwmJIdf5IA9sSWhm",
 			     "https://open.spotify.com/track/1R08nQA87DklgMg9h0nVK5",
 			     "https://open.spotify.com/track/3Q3myFA7q4Op95DOpHplaY",
-			     "https://open.spotify.com/track/0qgZvYWLIqDn0tXimF8gPa"
+			     "https://open.spotify.com/track/0qgZvYWLIqDn0tXimF8gPa",
+		     	     "https://open.spotify.com/track/1caUjUoE2lm6x9RDY7YtHc?si=J3iwkbdpSWCfW0ASyPwNmw",
+		     	     "https://open.spotify.com/track/2sPUMVRLMSHWdqJHzmZaLC?si=ghIgb077SvmbVX-VKjmAog",
+		     	     "https://open.spotify.com/track/6ZNo7Vi0TE9ul1fhKd4S1M?si=7YiEfmqOS4W9ZWNCNzDZSA"
 
 
 				];
@@ -67,7 +87,13 @@ function hyped(){
 				"https://open.spotify.com/track/6y3ijys1nhU1KCpy4mGED8",
 				"https://open.spotify.com/track/4lqEFIB9YZl8BwR99L5H3b",
 				"https://open.spotify.com/track/2BF1X5nkYYSb0AUTRaGkDm",
-				"https://open.spotify.com/track/4ipnJyDU3Lq15qBAYNqlqK"
+				"https://open.spotify.com/track/4ipnJyDU3Lq15qBAYNqlqK",
+		     	        "https://open.spotify.com/track/2xTft6GEZeTyWNpdX94rkf?si=vDlqIxKVS5y96pIt71IQ9g",
+		        	"https://open.spotify.com/track/3ssX20QT5c3nA9wk78V1LQ?si=JP1bX5EwTvSKQ31GfMnU0A",
+		     		"https://open.spotify.com/track/2KU0RHzEYq8GLwDTyimWGr?si=jyf22uwNQ7-tRZqNJXzwhA",
+		     		"https://open.spotify.com/track/4mIgMODzoKcvBnHeehLctZ?si=qxA-5Yb1SHWY9kVQb4Ufkw",
+		     		"https://open.spotify.com/track/2ZuhFZlNKF2O4Azj4clGfV?si=OFMSBFP2SfKP3EE0bmkxFA",
+		     		"https://open.spotify.com/track/7dFikRPQuLFz4PdCw4ZvCy?si=Fk7HDoNSTkq5ufSSegA8dw"
 				];
 	var randomNumber = Math.floor(Math.random() * (songs.length));
 	var song = songs[randomNumber];
@@ -88,7 +114,12 @@ function chill(){
 				 "https://open.spotify.com/track/06JRkix5RRMRIWffxZqlGt",
 				 "https://open.spotify.com/track/5jnp8E61wcoF6qIEtDHQnJ",
 				 "https://open.spotify.com/track/1LeItUMezKA1HdCHxYICed",
-				 "https://open.spotify.com/track/0vtX8UMG38p7IXpP4lZJ2z"
+				 "https://open.spotify.com/track/0vtX8UMG38p7IXpP4lZJ2z",
+		     		 "https://open.spotify.com/track/0QZ5yyl6B6utIWkxeBDxQN?si=JdGKSQtGSvqHdtWmMhspVg",
+		     		 "https://open.spotify.com/track/3v7yF5uFNoXph8br31ezj8?si=DRHhV9PDRAi4wtmAItBWtg",
+		     		 "https://open.spotify.com/track/61QRRv0qBswOpk5mQXlzu4?si=xJYGoJbkTv6TeOqSd-7nOQ",
+		     		 "https://open.spotify.com/track/5Y9Hqg5Z3EHUDDwd54FDQb?si=w_i22RoUROm5DjiAkhSKkg",
+		     		 "https://open.spotify.com/track/0DJBgBiYeSn6n1AXAkFVE8?si=ozAktBumStmPML_KH2qECw"
 
 
 				];
@@ -105,7 +136,14 @@ function idk(){
 				 "https://open.spotify.com/track/6b2oQwSGFkzsMtQruIWm2p",
 				 "https://open.spotify.com/track/0JXXNGljqupsJaZsgSbMZV",
 				 "https://open.spotify.com/track/16Me2jlMlTX4xsps2j71mC",
-				 "https://open.spotify.com/track/1CGMbXotw37W2TGOc16Kdj"
+				 "https://open.spotify.com/track/1CGMbXotw37W2TGOc16Kdj",
+		     		 "https://open.spotify.com/track/18i9mG58WcAWiRP68DYanN?si=i2FLpY_QTQWH6qq1azPVDg",
+		     		 "https://open.spotify.com/track/3LhSJiDkjo19tgnGi9Zdhq?si=VtCNqVQsTZ-kxjs0Cuiw_g",
+		     		 "https://open.spotify.com/track/6b2oQwSGFkzsMtQruIWm2p?si=_FmNin3YTkeNs2WOee3QLQ",
+		     		 "https://open.spotify.com/track/11LmqTE2naFULdEP94AUBa?si=4450fvUOTGyXC4yxMiWjfQ",
+		     		 "https://open.spotify.com/track/5LQqc2rfmxKJY1kuCDM2ml?si=oCupG41fS-WWEjKVjwxSPg",
+		     
+		     	
 
 
 				];
